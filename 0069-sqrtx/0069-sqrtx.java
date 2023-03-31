9d00cc9c2363f2fc21c31061d10549b0f10e93a5
Gsingh1575 <108575808+Gsingh1575@users.noreply.github.com>
2023-03-31 23:41:23 +0530

    Time: 1 ms (100.00%), Space: 39.9 MB (35.45%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int mySqrt(int x) {
+    int results = 0;
+	if (x == 0) {
+		return 0;
+	} else {
+		results = (int) Math.sqrt(x);
+	}
+	return results;
+ }
+        
+    
+}
